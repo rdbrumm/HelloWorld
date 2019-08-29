8d0720706e812ba894802b7243a48990d2537ec7
Rob Brumm <rdbrumm@gmail.com>
2019-08-29 10:01:56 -0700

    inserted another print line

--- src/Demo.java
+++ src/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args)
     {
         System.out.println("Hello from PACCAR!");
+        System.out.println("What it do baybeee!");
     }
 }
